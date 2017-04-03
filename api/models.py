5331dceb7b2d883328cbc46254d1c0e7b6e25e18
Ankit Shah <ank.h.shah@gmail.com>
2017-04-03 17:36:33 -0400

    Jaunt Model

--- api/models.py
+++ api/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+
 class Jaunt(models.Model):
-    start = models.DateTimeField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    live = models.BooleanField()
+    owner = models.IntegerField()
+    title = models.CharField(max_length=256)
+    shortcode = models.CharField(max_length=32)
+
