8a314b1d5eb0340ab2b63274ca9d92183e6a9cf4
Ankit Shah <ank.h.shah@gmail.com>
2017-04-04 22:14:22 -0400

    Creation of Jaunt objects working

--- api/models.py
+++ api/models.py
@@ -7,19 +7,19 @@ class Jaunt(models.Model):
     start_date = models.DateTimeField(null=True)
     end_date = models.DateTimeField(null=True)
     live = models.BooleanField(default=False)
-    owner = models.IntegerField()
+    owner = models.CharField(max_length=64)
     title = models.CharField(max_length=256)
     shortcode = models.CharField(max_length=32)
 
 
 class Membership(models.Model):
-    user_id = models.IntegerField()
+    user_id = models.CharField(max_length=64)
     jaunt = models.ForeignKey(Jaunt, on_delete=models.CASCADE)
 
 
 class Photo(models.Model):
     id = models.IntegerField(primary_key=True)
-    owner = models.IntegerField()
+    owner = models.CharField(max_length=64)
     original_url = models.CharField(max_length=512)
     thumbnail_url = models.CharField(max_length=512)
     taken_at = models.DateTimeField()
--- api/routes.py
+++ api/routes.py
@@ -1,24 +1,31 @@
+from copy import copy
+import json
 import pickle
 
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from api.models import Jaunt
 
 
 def get_next_shortcode():
-    with open('words.txt', 'rb') as f:
+    with open('api/words.txt', 'rb') as f:
         words_list = pickle.loads(f.read())
 
-    with open('words.txt', 'wb') as f:
+    with open('api/words.txt', 'wb') as f:
         to_return = words_list.pop()
         f.write(pickle.dumps(words_list))
         return to_return
 
-
+@csrf_exempt
 def create_jaunt(request):
-    if (request.method == 'POST'):
-        params_dict = request.POST
-        params_dict['shortcode'] = get_next_shortcode()
-        new_obj = Jaunt.objects.create(params_dict)
-        return JsonResponse({'id': new_obj})
+    if request.method == 'POST':
+        params_dict = json.loads(request.body)
+        shortcode = get_next_shortcode()
+        params_dict['shortcode'] = shortcode
+        new_obj = Jaunt.objects.create(**params_dict)
+        return JsonResponse({
+            'id': new_obj.id,
+            'shortcode': shortcode
+        })
 
